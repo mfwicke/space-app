e16e76c9ab20296c3a95e7dbfd24711e5336a0ef
Miguel <mwicke@gmail.com>
2022-11-08 11:39:53 +0100

    planets 1.5

--- client/src/components/planet/Planet.jsx
+++ client/src/components/planet/Planet.jsx
@@ -4,15 +4,12 @@ import "./Planet.css";
 export const Planet = ({ planet }) => {
   return (
     <>
-      <div className="solar">
-        <div className={"solar_system"}>
-          <div className={"planet " + planet.name}>
-            <div className={"planet_description"}>
-              {/* <h2>Planet</h2> */}
-              <h1>{`${planet.name}`}</h1>
-              <p className={""}>{planet.description}</p>
-            </div>
-            {/* <div className="overlay"></div> */}
+      <div className={"planet_description"}>
+        <div className="solar">
+          <h1>{`${planet.name}`}</h1>
+          <p className={""}>{planet.description}</p>
+          <div className={"solar_system rotate"}>
+            <div className={"planet " + planet.name}></div>
           </div>
         </div>
       </div>
