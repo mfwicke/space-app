173bc27d19526551b572b89736079d0d8193889f
Miguel <mwicke@gmail.com>
2022-11-24 14:30:00 +0100

    server die

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,40 +1,59 @@
-import React, { useState } from "react"; 
-import "./login.css"; 
-import axios from "axios"; 
+import React, { useState } from "react";
+import "./login.css";
+import axios from "./../../util/axiosApiInstance.js";
 
 export const Login = (props) => {
-    const [email, setEmail] = useState("");
-    const [pass, setPass] = useState(""); 
-    const [errMsg, setErrMsg] = useState('');
-    const [success, setSuccess] = useState(false); //use react router to navigate to a page
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false); //use react router to navigate to a page
 
-    const url = "http://localhost:3000/login/"   
+  const url = "/api/users/login/";
 
-    const handleSubmit = async (e) => {
-
-        e.preventDefault();
-        try {
-            const response = await axios.post(url, {email, pass})            
-        } catch (error) {
-            console.log(error); 
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(url, { email, password });
+    } catch (error) {
+      console.log(error);
     }
-    return (
+  };
+  return (
     <div className="outer-container">
-        <div className="auth-form-container">
-            <h2>Login</h2>
-            <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="email">email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="budspencer@gmail.com" id="email" name="email" />
-                <label htmlFor="password">password</label>
-                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                <button type="submit" id="button">Log In</button>
-            </form>
-            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
-        </div>
+      <div className="auth-form-container">
+        <h2>Login</h2>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <label htmlFor="email">email</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="budspencer@gmail.com"
+            id="email"
+            name="email"
+          />
+          <label htmlFor="password">password</label>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="********"
+            id="password"
+            name="password"
+          />
+          <button type="submit" id="button">
+            Log In
+          </button>
+        </form>
+        <button
+          className="link-btn"
+          onClick={() => props.onFormSwitch("register")}
+        >
+          Don't have an account? Register here.
+        </button>
+      </div>
     </div>
-        
-    )
-}
+  );
+};
 
 export default Login;
