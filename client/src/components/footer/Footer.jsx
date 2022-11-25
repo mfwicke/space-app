8e1de51f590ff6f50ff2a877f54034138cf80f0d
Miguel <mwicke@gmail.com>
2022-11-25 22:31:54 +0100

    changes

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./footer.css";
 import { NavLink, useNavigate } from "react-router-dom";
-import {FaInstagramSquare} from "@react-icons/all-files/fa/FaInstagramSquare";
-import {FaSnapchatSquare} from "@react-icons/all-files/fa/FaSnapchatSquare";
-import {FaTwitterSquare} from "@react-icons/all-files/fa/FaTwitterSquare";
-import {FaFacebookSquare} from "@react-icons/all-files/fa/FaFacebookSquare";
+import { FaInstagramSquare } from "@react-icons/all-files/fa/FaInstagramSquare";
+import { FaSnapchatSquare } from "@react-icons/all-files/fa/FaSnapchatSquare";
+import { FaTwitterSquare } from "@react-icons/all-files/fa/FaTwitterSquare";
+import { FaFacebookSquare } from "@react-icons/all-files/fa/FaFacebookSquare";
 
 export default function Footer() {
   const navigate = useNavigate();
@@ -13,23 +13,38 @@ export default function Footer() {
     navigate("/about", "/contacts", "/register", "/login", "/user");
   };
 
-
   return (
     <footer className="footerBox">
       <div className="social">
-        <NavLink to="/"><FaInstagramSquare /></NavLink>
-        <NavLink to="/"><FaSnapchatSquare /></NavLink>
-        <NavLink to="/"><FaTwitterSquare /></NavLink>
-        <NavLink to="/"><FaFacebookSquare /></NavLink>
+        <NavLink to="/">
+          <FaInstagramSquare />
+        </NavLink>
+        <NavLink to="/">
+          <FaSnapchatSquare />
+        </NavLink>
+        <NavLink to="/">
+          <FaTwitterSquare />
+        </NavLink>
+        <NavLink to="/">
+          <FaFacebookSquare />
+        </NavLink>
       </div>
       <ul className="footerList">
-        <li><NavLink to="/">Job Opportunities</NavLink></li>
-        <li><NavLink to="/">Services</NavLink></li>
-        <li><NavLink to="/">Terms & Conditions</NavLink></li>
-        <li><NavLink to="/">Privacy Policy</NavLink></li>
+        <li>
+          <NavLink to="/">Job Opportunities</NavLink>
+        </li>
+        <li>
+          <NavLink to="/">Services</NavLink>
+        </li>
+        <li>
+          <NavLink to="/">Terms & Conditions</NavLink>
+        </li>
+        <li>
+          <NavLink to="/">Privacy Policy</NavLink>
+        </li>
       </ul>
       <div className="credits">
-      <h6>Copyright © 2022 STELLAR TRIPS Inc. All rights reserved.</h6>
+        <h6>Copyright © 2022 STELLAR TRIPS Inc. All rights reserved.</h6>
       </div>
     </footer>
   );
