a17465bd13bb459e4fcec27fd9074768548f68e6
vincenzoarena <penninoematita@gmail.com>
2022-11-28 21:44:04 +0100

    New favicon.ico created and first part of styling on the Contacts component

---
+++ client/src/assets/JSON/contactsInfo.js
@@ -0,0 +1,44 @@
+export const contacts = [
+  {
+    name: "STELLAR TRIPS Inc.",
+    id: 0,
+    image: "../images/stlogo-white.png",
+    description: "We are a travel agency that specializes in space travel.",
+    age: "Less than one year",
+    info: "2140, Earth has become the base of operation for the colonization of the solar system. The new discovery in the field of space travels, new impulse and warp engines, artificial gravity generators, teleportation devices, made for Humankind easier to finally discover and start connecting with new intelligent life forms. The new technologies in the field of human clonation, and recording, storing and implanting of cerebral patterns and memories, opened up the doors of a new endless life for all humanity. In this year three men from the 21st century were brought back and they met at a convention on new technologies held by the Mars Colonial Society, a company who was building the first orbital station around the red planet. After talking together about their ideas on the possibilities for the development of the colonization process and his commercial potential, they decided to found STELLAR TRIPS Inc. December 13th 2143 was the inauguration day of STELLAR TRIPS Inc. first office on Earth, in Berlin, Germany. Few months later the three founders, Miguel Wicke, Lorenz Schmid and Vincenzo Arena, presented their programs to build orbital space stations around each planet of the solar system and, in some cases, also around some of their moons. Next step would have been building bases on the planets’ surface. Since then billions of tourists used our company services to enjoy the life on other planets who are now some of the most beloved holidays location, offering many different attractions, activities and accommodations.",
+    github: "",
+    linkedin: "",
+    email: "contactus@stellartrips.com",
+  },
+
+  {
+    name: "Lorenz Schmid",
+    id: 1,
+    image: "../images/dci-martians-lorenz-schmid.png",
+    info: "lorem ipsum dolor sit amet",
+    github: "https://github.com/Lorenzzschmid",
+    linkedin: "https://www.linkedin.com/in/lorenz-schmid-0a8b8a24b/",
+    email: "mighty.lo.schmid@gmail.com",
+  },
+
+  {
+    name: "Vincenzo Arena",
+    id: 2,
+    image: "../images/dci-martians-vincenzo-arena.png",
+    info: "Born originally in a small town in Southern Italy in the last twenty years of the 20th century, Vincenzo Arena started pretty soon to get himself into arts and science. Deeply in love with animation, movies, video games and comics. His passion for puzzles and building blocks, and all the time spent in front of a computer, led him to Web Development, after moving to Berlin in 2013.",
+    github: "https://github.com/vincenzoarena/",
+    linkedin: "https://www.linkedin.com/in/vincenzo-arena-032a064b/",
+    email: "penninoematita@gmail.com",
+  },
+
+  {
+    name: "Miguel Wicke",
+    id: 3,
+    image: "../images/dci-martians-miguel-wicke.png",
+    info: "After many years wandering around the new world, going in and out of the cryogenic chamber living on slow planets, so that the years would not wreak havoc, he managed to reconnect with Loren and Vince, whom he had not seen since the beginning of the destruction for the years of the pandemic and war when, due to things in life they took a course together that brought them to what they do today. His experience in tourism added to Vince's engineering and Lorenz's ideas and ease of solving problems brought them to this point of forming this great intergalactic company in which they put together their passions and our hobbies, so that the rest of humanity can enjoy without limitations of outer space! Happy journey",
+    github: "https://github.com/mfwicke/",
+    linkedin: "https://www.linkedin.com/in/miguelwickeld/",
+    email: "mwicke@gmail.com",
+  },
+];
+
