e8b8812c666662796f4b4e5bd182c4125984a633
Miguel <mwicke@gmail.com>
2022-11-30 14:44:57 +0100

    user is here

--- client/src/components/user/User.jsx
+++ client/src/components/user/User.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Loading from "../loading/Loading";
 
 const User = (props) => {
   const [user, setUser] = useState(null);
+  const navigate = useNavigate();
 
   const url = "/api/users/profile/:id";
 
@@ -12,6 +15,7 @@ const User = (props) => {
       try {
         const response = await axios.get(url, { user });
         setUser(response.data);
+        navigate("/profile");
       } catch (error) {
         console.log(error);
       }
