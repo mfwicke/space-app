25fe468e4d1553ef2f6f7cad69a2f943e74b5cf3
vincenzoarena <penninoematita@gmail.com>
2022-12-07 16:19:03 +0100

    Minor fixing

--- client/src/components/package-bookin-form/PackageBooking.jsx
+++ client/src/components/package-bookin-form/PackageBooking.jsx
@@ -1,91 +1,94 @@
-import React, {useState} from 'react'; 
-import { planets } from '../../assets/JSON/planetsObj';
-import './packageBooking.css'; 
-
-
+import React, { useState } from "react";
+import { planets } from "../../assets/JSON/planetsObj";
+import "./packageBooking.css";
 
 export const PackageBooking = () => {
-
   const [isOpen, setOpen] = useState(false);
   const [items, setItem] = useState(planets);
   const [selectedItem, setSelectedItem] = useState(null);
 
-  const [selectedPlanet, setSelectedPlanet] = useState(null); 
-  
+  const [selectedPlanet, setSelectedPlanet] = useState(null);
+
   const toggleDropdown = () => setOpen(!isOpen);
 
-  
-  
   const handleItemClick = (name) => {
-    console.log("I got clicked", name)
+    console.log("I got clicked", name);
     selectedItem == name ? setSelectedItem(null) : setSelectedItem(name);
-    
+
     //useNavigate in here
-    setOpen(false); 
-  }
- 
+    setOpen(false);
+  };
+
   const handleProceed = (e) => {
     e.preventDefault();
     console.log(selectedItem);
-    if(selectedItem !== null) {
-      const findPlanet = planets.find((planet) => planet.name === selectedItem); 
-      setSelectedPlanet(findPlanet); 
-      console.log(findPlanet)
-    } 
-  }
+    if (selectedItem !== null) {
+      const findPlanet = planets.find((planet) => planet.name === selectedItem);
+      setSelectedPlanet(findPlanet);
+      console.log(findPlanet);
+    }
+  };
 
   return (
     <>
       <div className="booking-form-container">
-
-        <h1 id='welcome-message'>Welcome Traveler Book Your Planet</h1>
-      
-      
+        <h1 id="welcome-message">Welcome Traveler Book Your Planet</h1>
 
         <div className="dropdown-box">
-
-          <div className='dropdown-header' onClick={toggleDropdown}>
-              {selectedItem ? selectedItem : "Select your Planet:"}
+          <div className="dropdown-header" onClick={toggleDropdown}>
+            {selectedItem ? selectedItem : "Select your Planet:"}
           </div>
-
-          <div className={`dropdown-body ${isOpen && 'open'}`}>
-            {items.map(item => (
-              <div className="dropdown-item" onClick={() => handleItemClick(item.name)} key={item.name} name={item.name}>
-                <span className={`dropdown-item-dot ${item.name == selectedItem && 'selected'}`}></span>
-                {item.name} 
+          <div className={`dropdown-body ${isOpen && "open"}`}>
+            {items.map((item) => (
+              <div
+                className="dropdown-item"
+                onClick={() => handleItemClick(item.name)}
+                key={item.name}
+                name={item.name}
+              >
+                <span
+                  className={`dropdown-item-dot ${
+                    item.name == selectedItem && "selected"
+                  }`}
+                ></span>
+                {item.name}
               </div>
-              ))}
+            ))}
           </div>
-
-          {selectedPlanet !== null ? 
-                <div className="redirect-form-container">
-                  <h1 id='redirect-message'>{`Welcome to ${selectedPlanet.name}`}</h1>
-                  <div className="info">
-                    {selectedPlanet.package.map((item => 
-                      <h4 className='items'>
-                      Package: {item.name}, 
-                      Price: {item.price}
-                      Description: {item.description}
-                     <input type="checkbox" />
-                     <section className="checkbox">
-                     </section>
-                      </h4>
-
-                    ))}
-                  </div>
-                    <span className="btn-book">
-                      <button type="submit" id="package-redirect-button">Book</button>
-                  </span>        
-                </div>
-                : ""}; 
-          
+          {selectedPlanet !== null ? (
+            <div className="redirect-form-container">
+              <h1 id="redirect-message">{`Welcome to ${selectedPlanet.name}`}</h1>
+              <div className="info">
+                {selectedPlanet.package.map((item) => (
+                  <>
+                    <h4 className="items"><input type="checkbox" />{` Package: ${item.name}, 
+                      Price: ${item.price}, 
+                      Description: ${item.description}`}</h4>
+                  </>
+                ))}
+              </div>
+              <span className="btn-book">
+                <button type="submit" id="package-redirect-button">
+                  Book
+                </button>
+              </span>
+            </div>
+          ) : (
+            ""
+          )}
+          ;
         </div>
-        
-          <span className="dropdown-package-proceed">
-            <button type="submit" id="package-dropdown-button" onClick={handleProceed} >Proceed</button>
-          </span>
-        
+
+        <span className="dropdown-package-proceed">
+          <button
+            type="submit"
+            id="package-dropdown-button"
+            onClick={handleProceed}
+          >
+            Proceed
+          </button>
+        </span>
       </div>
     </>
-  )
-}
+  );
+};
